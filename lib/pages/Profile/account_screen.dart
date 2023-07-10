f4c136855aa2b450f04ca9a8ef7c2a4da3a646a1
raziruu <shrestharajill11@gmail.com>
2023-07-10 10:44:05 +0545

    profile screen

--- lib/pages/Profile/account_screen.dart
+++ lib/pages/Profile/account_screen.dart
@@ -15,6 +15,7 @@ class AccountScreen extends StatefulWidget {
   @override
   State<AccountScreen> createState() => _AccountScreenState();
 }
+
 class _AccountScreenState extends State<AccountScreen> {
   void logout() async {
     _ui.loadState(true);
@@ -42,3 +43,33 @@ class _AccountScreenState extends State<AccountScreen> {
     _ui.loadState(false);
   }
 
+  late GlobalUIViewModel _ui;
+  late AuthViewModel _auth;
+  @override
+  void initState() {
+    _ui = Provider.of<GlobalUIViewModel>(context, listen: false);
+    _auth = Provider.of<AuthViewModel>(context, listen: false);
+    super.initState();
+  }
+
+  double rating = 0;
+  createRating() {
+    return SizedBox(
+      child: RatingBar.builder(
+        initialRating: rating,
+        minRating: 1,
+        itemSize: 35,
+        itemPadding: EdgeInsets.symmetric(horizontal: 1),
+        itemBuilder: (context, _) => Icon(
+          CupertinoIcons.heart_fill,
+          color: Colors.redAccent,
+        ),
+        updateOnDrag: true,
+        onRatingUpdate: (rating) => setState(() {
+          this.rating = rating;
+        }),
+      ),
+    );
+  }
+
+ 
