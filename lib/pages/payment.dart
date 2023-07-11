45d7652516f9a56906d77985201c438f0eea5542
Rajeev9813 <razeev.mahat2058@gmail.com>
2023-07-11 19:49:54 +0545

    Payment Credentials Page

---
+++ lib/pages/payment.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jutta_junction/customs/custom_icons.dart';
+import 'package:khalti_flutter/khalti_flutter.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class Payment extends StatefulWidget {
+  const Payment({Key? key}) : super(key: key);
+
+  @override
+  _PaymentState createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  String referenceId = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new)
+              .iconColor(Colors.black)
+              .box
+              .make(),
+          onPressed: () {
+            Navigator.pushNamed(context, "/NewHomePage");
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Payment Methods",
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Color.fromARGB(255, 92, 15, 163),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Divider(
+                color: Color.fromARGB(255, 92, 15, 163),
+                height: 20,
+                thickness: 1,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              //Image for Khalti
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  print("Image Tapped");
+                  khaltiWallet();
+                }, // Image tapped
+                child: Image.asset(
+                  'assets/images/khaltilogo.png',
+                  width: 180,
+                ),
+              ),
+
+              SizedBox(
+                height: 40,
+              ),
+
+              //Cash on Delivery
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  AlertDialog(
+                    title: const Text("Cash on Delivery"),
+                    content: const Text("Payment successful"),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("OK"))
+                    ],
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/cod.png',
+                  width: 100,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              InkWell(
+                splashColor: Colors.grey,
+                onTap: () {
+                  AlertDialog(
+                    title: const Text("Cash on Delivery"),
+                    content: const Text("Payment successful"),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("OK"))
+                    ],
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/visa.png',
+                  width: 150,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Divider(
+                color: Color.fromARGB(255, 92, 15, 163),
+                height: 20,
+                thickness: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  khaltiWallet() {
+    KhaltiScope.of(context).pay(
+      config: PaymentConfig(
+          amount: 1000,
+          productIdentity: "1",
+          productName: "nike blazers mid 77",
+          mobile: "9804704028"),
+      preferences: [PaymentPreference.khalti],
+      onSuccess: onSuccess,
+      onFailure: onFailure,
+      onCancel: onCanceled,
+    );
+  }
+
+  // mentod to handle success
+  void onSuccess(PaymentSuccessModel success) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Success"),
+            content: Text("Payment successful"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("OK"),
+              ),
+            ],
+          );
+        });
+  }
+
