91d4a566abbe94b1c7bfad09725dd37f7a8a0c1f
krishalsatta <sattakrishal97@gmail.com>
2023-07-12 15:16:54 +0545

    Otp verification

---
+++ lib/pages/OtpVerification.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+// import 'package:jutta_junction/customs/custom_icon.dart';
+import 'package:jutta_junction/customs/custom_icons.dart';
+import 'package:jutta_junction/pages/ChangePassword.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
