89f8ddf3e7b6775af5321bbe48d2c25049908ae8
krishalsatta <sattakrishal97@gmail.com>
2023-07-13 07:36:14 +0545

    otp verification on progress

--- lib/pages/OtpVerification.dart
+++ lib/pages/OtpVerification.dart
@@ -4,3 +4,10 @@ import 'package:jutta_junction/customs/custom_icons.dart';
 import 'package:jutta_junction/pages/ChangePassword.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
+
+class OtpVerify extends StatefulWidget {
+  const OtpVerify({super.key});
+
+  @override
+  State<OtpVerify> createState() => _OtpVerifyState();
+}
