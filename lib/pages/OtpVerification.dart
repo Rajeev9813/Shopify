b9d56b16c15a174f17d04a52fcb1b26c9d57340d
krishalsatta <sattakrishal97@gmail.com>
2023-07-14 22:07:05 +0545

    otp verification completed

--- lib/pages/OtpVerification.dart
+++ lib/pages/OtpVerification.dart
@@ -31,3 +31,82 @@ class _OtpVerifyState extends State<OtpVerify> {
           ),
         ),
       ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 50, bottom: 20),
+              child: Text("Verify OTP",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                  )),
+            ),
+            Text(
+              "Please enter 5 digit OTP code",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(height: 30),
+            PinCodeTextField(
+              length: 5,
+              obscureText: false,
+              appContext: context,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(5),
+                fieldHeight: 58,
+                fieldWidth: 58,
+                activeFillColor: Colors.white,
+                activeColor: Colors.red,
+                inactiveColor: Colors.white,
+                inactiveFillColor: Colors.white,
+                selectedColor: Colors.red,
+                selectedFillColor: Colors.white,
+              ),
+              animationDuration: Duration(milliseconds: 300),
+              enableActiveFill: true,
+              onCompleted: (v) {
+                print("Completed");
+              },
+              onChanged: (value) {},
+            ),
+            SizedBox(height: 20),
+            CustomButton(
+              title: "Verify OTP",
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChangePassword(),
+                    ));
+              },
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              child: RichText(
+                  text: TextSpan(
+                      text: "Didn't received any code ?",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 61, 60, 60),
+                      ),
+                      children: [
+                    TextSpan(
+                      text: "Resend code",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ])),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
