c90344107174ad20340a9e0f820327162fefed40
BibekKumarKushwaha <bibekkumar345@gmail.com>
2023-07-31 13:02:36 +0545

    minor twicks

--- lib/pages/Profile/change_your_email.dart
+++ lib/pages/Profile/change_your_email.dart
@@ -265,7 +265,7 @@ class _ChangeEmailState extends State<ChangeEmail> {
             // Add your code for logging out here
           },
           style: ElevatedButton.styleFrom(
-            primary: Colors.purple,
+            primary: Colors.red,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(28),
             ),
