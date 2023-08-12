0bdd8df3a630f66848bd37eaeb5c97c40eaa4f49
raziruu <shrestharajill11@gmail.com>
2023-08-12 22:38:48 +0545

    new commit

--- lib/pages/product_Detail/Product_Detail.dart
+++ lib/pages/product_Detail/Product_Detail.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:jutta_junction/Controller/cart_controller.dart';
+import 'package:jutta_junction/pages/product_Detail/body.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
