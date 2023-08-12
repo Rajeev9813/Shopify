e90900b0ce05d0fa566470611c39c0a6db97df3e
sarukhadka1 <saru.khadka521@gmail.com>
2023-08-12 12:53:25 +0545

    ratingreview

---
+++ lib/repositories/ratingreviewrepo.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/ratingreview_model.dart';
+import '../services/firebase_service.dart';
+
+class RatingReviewRepository{
+  CollectionReference<RatingReviewModel> ref =
+  FirebaseService.db.collection("ratingreview")
+      .withConverter<RatingReviewModel>(
+    fromFirestore: (snapshot,_) {
+      return RatingReviewModel.fromFirebaseSnapshot(snapshot);
+    },
+    toFirestore: (model, _) => model.toJson(),
+  );
