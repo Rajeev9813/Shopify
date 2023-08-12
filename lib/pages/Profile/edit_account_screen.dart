1b14064ae1461364ea935b0b0e498b52d6f1ce0c
raziruu <shrestharajill11@gmail.com>
2023-08-12 22:32:57 +0545

    new commit

--- lib/pages/Profile/edit_account_screen.dart
+++ lib/pages/Profile/edit_account_screen.dart
@@ -1,15 +1,1 @@
 import 'package:flutter/cupertino.dart';
-
-class EditAccountScreen extends StatefulWidget {
-  const EditAccountScreen({Key? key}) : super(key: key);
-
-  @override
-  State<EditAccountScreen> createState() => _EditAccountScreenState();
-}
-
-class _EditAccountScreenState extends State<EditAccountScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
