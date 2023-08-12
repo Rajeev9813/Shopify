807225e4b74233784d1fb3cd0556e0c3d1668433
BibekKumarKushwaha <bibekkumar345@gmail.com>
2023-08-12 16:10:43 +0545

    test

---
+++ test/loginnn_test.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class AuthRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthRepository({required FirebaseAuth firebaseAuth})
+      : _firebaseAuth = firebaseAuth;
+
+  Future<UserCredential> login(String email, String password) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception('Failed to log in: ${e.message}');
+    }
+  }
+}
