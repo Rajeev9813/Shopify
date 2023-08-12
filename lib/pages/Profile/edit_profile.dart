a234e98330ef1f475329918b665ac3fd7e88c9e9
sarukhadka1 <saru.khadka521@gmail.com>
2023-08-12 12:30:19 +0545

    edit profile

---
+++ lib/pages/Profile/edit_profile.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../../viewmodels/auth_viewmodel.dart';
+
+class ProfileInfo extends StatefulWidget {
+  ProfileInfo({Key? key}) : super(key: key);
+
+  @override
+  _ProfileInfoState createState() => _ProfileInfoState();
+}
+
+class _ProfileInfoState extends State<ProfileInfo> {
+  late AuthViewModel _authViewModel;
