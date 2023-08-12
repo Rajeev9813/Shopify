14ddab07ed95af10e0d08001f94c6d7f266f65c9
BibekKumarKushwaha <bibekkumar345@gmail.com>
2023-08-12 20:45:39 +0545

    auth repository

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -3,3 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/user_model.dart';
 import '../services/firebase_service.dart';
+class AuthRepository {
+  CollectionReference<UserModel> userRef =
+  FirebaseService.db.collection("users").withConverter<UserModel>(
+    fromFirestore: (snapshot, _) {
+      return UserModel.fromFirebaseSnapshot(snapshot);
+    },
+    toFirestore: (model, _) => model.toJson(),
+  );
