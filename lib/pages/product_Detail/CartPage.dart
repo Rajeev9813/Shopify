1c0dd85ecfd2730b78deb5f0447880dc70a49f82
krishalsatta <sattakrishal97@gmail.com>
2023-08-13 21:57:11 +0545

    commit

--- lib/pages/product_Detail/CartPage.dart
+++ lib/pages/product_Detail/CartPage.dart
@@ -5,31 +5,31 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'cart_total.dart';
 import 'cartproducts.dart';
 
-// class CartPage extends StatelessWidget {
-//   const CartPage({super.key});
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       resizeToAvoidBottomInset: false,
-//       appBar: AppBar(
-//         elevation: 0.0,
-//         title: Text(
-//           "Your Cart",
-//           style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-//         ),
-//         backgroundColor: Colors.grey,
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           CartProducts(),
-//           CartTotal(),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text(
+          "Your Cart",
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CartProducts(),
+          CartTotal(),
+        ],
+      ),
+    );
+  }
+}
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
