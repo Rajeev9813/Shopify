0f7cc85a48013de9b359ab5bf20c752f4eb44ea8
krishalsatta <sattakrishal97@gmail.com>
2023-08-13 21:54:52 +0545

    commit

--- lib/pages/qr/qrhomepage.dart
+++ lib/pages/qr/qrhomepage.dart
@@ -14,60 +14,71 @@ class QrHomePage extends StatefulWidget {
 }
 
 class _QrHomePageState extends State<QrHomePage> {
- 
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	//Appbar having title
-	appBar: AppBar(
-		title: Center(child: Text("QR Code")),
-	),
-	
-	body: Container(
-		width: 500,
-		height: 500,
-		child: Column(
-		mainAxisAlignment: MainAxisAlignment.center,
-		crossAxisAlignment: CrossAxisAlignment.stretch,
-		children: [
-			//Display Image
-			Image(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQyYwscUPOH_qPPe8Hp0HAbFNMx-TxRFubpg&usqp=CAU"),height: 50,),
-			
-			//First Button
-			ElevatedButton(
-			// padding: EdgeInsets.all(15),
-			onPressed: (){
-				Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ScanQR()));
-			},
-				child: Text("Scan QR Code",style: TextStyle(color: Color.fromARGB(255, 11, 11, 12)),),
-			style: ButtonStyle(
-			shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-				borderRadius: BorderRadius.circular(20),
-				side: BorderSide(color: Color.fromARGB(255, 19, 20, 23)),)
-      ),
-			),
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //Appbar having title
+        appBar: AppBar(
+          title: Center(child: Text("QR Code")),
+        ),
+        body: Container(
+          width: 500,
+          height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //Display Image
+              Image(
+                image: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQyYwscUPOH_qPPe8Hp0HAbFNMx-TxRFubpg&usqp=CAU"),
+                height: 50,
+              ),
 
-			// SizedBox(height: 10),
+              //First Button
+              ElevatedButton(
+                // padding: EdgeInsets.all(15),
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => ScanQR()));
+                },
+                child: Text(
+                  "Scan QR Code",
+                  style: TextStyle(color: Color.fromARGB(255, 11, 11, 12)),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: BorderSide(color: Color.fromARGB(255, 19, 20, 23)),
+                  )),
+                ),
+              ),
 
-			//Second Button
-			ElevatedButton(
-			// padding: EdgeInsets.all(15),
-			onPressed: (){
-				Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
-															GenerateQR()));
-			},
-			// child: Text("Generate QR Code", style: TextStyle(color: Color.fromARGB(255, 13, 13, 14)),),
-      // style: ButtonStyle(
-			// shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-			// 	borderRadius: BorderRadius.circular(20),
-			// 	side: BorderSide(color: Color.fromARGB(255, 23, 24, 27)),
-			// ),
-			// ),),)
-		],
-		),
-	)
-	);
+              // SizedBox(height: 10),
+
+              //Second Button
+              ElevatedButton(
+                // padding: EdgeInsets.all(15),
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GenerateQR()));
+                },
+                child: Text(
+                  "Generate QR Code",
+                  style: TextStyle(color: Color.fromARGB(255, 13, 13, 14)),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      side: BorderSide(color: Color.fromARGB(255, 23, 24, 27)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
 }
-}
-
