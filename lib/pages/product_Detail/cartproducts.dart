8f891c22dc94a67d7d24bd8e740d3fa9fcb99e86
Rajeev9813 <105190769+Rajeev9813@users.noreply.github.com>
2023-08-13 20:38:18 +0545

    removing a snaclbar from the page

--- lib/pages/product_Detail/cartproducts.dart
+++ lib/pages/product_Detail/cartproducts.dart
@@ -90,10 +90,7 @@ class CartProductCard extends StatelessWidget {
               onPressed: () {
                 controller.addProduct(product);
                    ScaffoldMessenger.of(context).showSnackBar(
-  SnackBar(
-    content: Text("Product is Added"
-      " You have added the ${product.title} to the cart",
-      textAlign: TextAlign.center,
+  
     ),
     duration: Duration(seconds: 2),
   ),
